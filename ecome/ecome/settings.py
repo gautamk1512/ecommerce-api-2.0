78ab620fb98ca2a7168cbdd508874b19ee9ee528
Gautam singh <gautamk1512>
2023-04-29 19:26:07 +0530

    2 commit

--- ecome/ecome/settings.py
+++ ecome/ecome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-t-i+a#uetm_mbtqe7o8o)atsx52fj@l$tew$80y9__g(*sg$d-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

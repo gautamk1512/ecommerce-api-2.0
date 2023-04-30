d074669ae21ec07d96c1a8546f5f1667e87a89eb
Gautam singh <gautamk1512>
2023-04-30 10:21:19 +0530

    update commit

--- ecome/api/urls.py
+++ ecome/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import ProductViewSet
-from api import views
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # path('', views.home, name='home'),
-    path('products/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve'})),
+    # List all products or create a new product
+    path('products/', include(router.urls)),
+    # Retrieve, update or delete a specific product
+    path('products/<int:pk>/', ProductViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
